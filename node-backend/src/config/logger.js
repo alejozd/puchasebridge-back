346e828fa6afcdecc13e86770cf1a66ee837cae7
alejozd <alejozd79@gmail.com>
2026-04-05 15:26:47 -0500

    quite la carpeta de node

--- node-backend/src/config/logger.js
+++
@@ -1,4 +0,0 @@
-const pino = require('pino');
-const env = require('./env');
-
-module.exports = pino({ level: env.logLevel });
--- node-backend/src/controllers/documentosController.js
+++
@@ -1,8 +0,0 @@
-const { procesarDocumentos } = require('../services/documentoService');
-
-const procesar = async (req, res) => {
-  const result = await procesarDocumentos({ files: req.body?.files || [], session: req.session });
-  return res.json(result);
-};
-
-module.exports = { procesar };
